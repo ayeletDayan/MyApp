afff7293299001f07f0b1e25bcf2d7e5d49dcd44
Ayelet Dayan <ayeletgeri@yahoo.com>
2021-11-14 08:03:17 +0200

    5th commit 14.11

--- js/mail/cmps/mail-nav.cmp.js
+++ js/mail/cmps/mail-nav.cmp.js
@@ -4,11 +4,11 @@ export default {
     props: ['mails'],
     template: `
         <ul class="mail-nav">
-            <li @click="inbox"> Inbox </li>
-            <li @click="starred"> Starred </li>
-            <li @click="sent"> Sent </li>
-            <li @click="draft"> Drafts </li>
-            <li @click="trash"> Trash </li>
+            <li @click="inbox" class="fa fa-envelope"> Inbox </li>
+            <li @click ="starred" class="fa fa-star"> Starred </li>
+            <li @click="sent" class="fa fa-paper-plane"> Sent </li>
+            <li @click="draft" class="fas fa-edit"> Drafts </li>
+            <li @click="trash" class="fa fa-trash"> Trash </li>
         </ul>
     `,
     methods: {
